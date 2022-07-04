313aae064823954ba2bd0eec68ee01427e8068d8
Mercynenoh <mercychepngenoh74@gmail.com>
2022-07-04 23:34:55 +0300

    Create workout component and routing for workouts and yoga

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,18 +4,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { YogaComponent } from './yoga/yoga.component';
+import { WorkoutsComponent } from './workouts/workouts.component';
+import { RouterModule } from '@angular/router';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     YogaComponent,
+    WorkoutsComponent,
   
    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      {path: '', component: YogaComponent},
+      {path: 'workouts', component: WorkoutsComponent},
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
