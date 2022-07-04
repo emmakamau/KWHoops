7b3f282ff1522ae83d7232d387420d9c88ca5064
Maureen-Muriithi <moh2wanja@gmail.com>
2022-07-04 14:49:09 +0300

    Create component diet

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DietComponent } from './components/diet/diet.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DietComponent
   ],
   imports: [
     BrowserModule,
