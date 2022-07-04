977ce2174430e23c1b7b36db38a95524ac5819d3
rossyvee <roseakinyi001@gmail.com>
2022-07-04 20:38:55 +0300

    creating an if statement for when the serializer is valid and returning response

--- wkhoops/authentication/views.py
+++ wkhoops/authentication/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 from rest_framework.generics import GenericAPIView
 from authentication.serializers import RegisterSerializer
-
+from rest_framework import response
 class RegisterAPIView(GenericAPIView):
     
     serializer_class = RegisterSerializer
     
     def post(self, request):
         serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return response.Response
 
