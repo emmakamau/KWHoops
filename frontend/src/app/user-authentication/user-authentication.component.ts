5ffb033a2b47ac18bd8c31faf9dc8d4fd09426d5
EmmaK <emmaculatewkamau@gmail.com>
2022-07-05 00:11:38 +0300

    Add styling

--- frontend/src/app/user-authentication/user-authentication.component.ts
+++ frontend/src/app/user-authentication/user-authentication.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+declare var loadExtJS: any;
+
 @Component({
   selector: 'app-user-authentication',
   templateUrl: './user-authentication.component.html',
@@ -10,6 +12,7 @@ export class UserAuthenticationComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    new loadExtJS();
   }
 
 }
