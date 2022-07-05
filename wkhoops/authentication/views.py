63119b64f5a423419140dfbcedee05577ef142c7
rossyvee <roseakinyi001@gmail.com>
2022-07-05 08:47:55 +0300

    updated the function for post to return an error if the post is invalid

--- wkhoops/authentication/views.py
+++ wkhoops/authentication/views.py
@@ -14,5 +14,5 @@ class RegisterAPIView(GenericAPIView):
         if serializer.is_valid():
             serializer.save()
             return response.Response(serializer.data, status=status.HTTP_201_CREATED )
-        return response.Response(serializer.data, status=status.HTTP_400_BAD_REQUEST )
+        return response.Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST )
 
