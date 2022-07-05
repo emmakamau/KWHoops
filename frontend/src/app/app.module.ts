926d4cb5af9eaead01a5eaa04cee6282a941d379
Maureen-Muriithi <moh2wanja@gmail.com>
2022-07-05 08:40:53 +0300

    Create bootcamp component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DietComponent } from './components/diet/diet.component';
+import { BootcampsComponent } from './components/bootcamps/bootcamps.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DietComponent
+    DietComponent,
+    BootcampsComponent
   ],
   imports: [
     BrowserModule,
---
+++ frontend/src/app/components/bootcamps/bootcamps.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bootcamps',
+  templateUrl: './bootcamps.component.html',
+  styleUrls: ['./bootcamps.component.css']
+})
+export class BootcampsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
