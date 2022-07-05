8859cee06ad47c7111ff051578e590796709cada
faith62 <phifeichemu@gmail.com>
2022-07-05 09:19:23 +0300

    add sibscription plan model

--- wkhoops/subscriptions/models.py
+++ wkhoops/subscriptions/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Subscription_Plan(models.Model):
+    sub_plan_name = models.CharField(max_length=50)
+    sub_plan_cost = models.IntegerField()
+
+    def __str__(self):
+        return self.sub_plan_name
+
+    def save_subsription_plan(self):
+        self.save() 
