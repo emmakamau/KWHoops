c5a794909669409b9ad55227620c36224388e285
Maureen-Muriithi <moh2wanja@gmail.com>
2022-07-05 23:27:08 +0300

    Refactor routing, logic and styling for the diet page

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BootcampsComponent } from './components/bootcamps/bootcamps.component';
+import { DietComponent } from './components/diet/diet.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'diet',
+    component: DietComponent
+  },
+  
+  {
+    path: 'bootcamps',
+    component: BootcampsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
