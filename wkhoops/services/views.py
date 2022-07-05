09eb4da2dd381ada7f60e55256b29364eb333f06
Fridah-kalee <fridahkalee@gmail.com>
2022-07-05 10:20:31 +0300

    create views that handles the logic of combining a model, serializer

--- wkhoops/services/views.py
+++ wkhoops/services/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Services,Category
+from rest_framework.viewsets import ModelViewSet
+from .serializers import ServicesSerializer,CategorySerializer
 
 # Create your views here.
+class ServicesViewSet(ModelViewSet):
+    queryset = Services.objects.all()
+    serializer_class = ServicesSerializer
+
+class CategoryViewSet(ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer    
+
