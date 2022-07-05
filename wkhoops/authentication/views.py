4da4fc635651b9dcc52bb5a12b7f046129ea1083
rossyvee <roseakinyi001@gmail.com>
2022-07-05 10:52:56 +0300

    created the login class and a post function that takes in email and password parameters

--- wkhoops/authentication/views.py
+++ wkhoops/authentication/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from rest_framework.generics import GenericAPIView
 from authentication.serializers import RegisterSerializer
 from rest_framework import response, status
-
+from django.contrib.auth import authenticate
 
 class RegisterAPIView(GenericAPIView):
     
@@ -15,4 +15,16 @@ class RegisterAPIView(GenericAPIView):
             serializer.save()
             return response.Response(serializer.data, status=status.HTTP_201_CREATED )
         return response.Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST )
+
+
+
+class LoginAPIView(GenericAPIView):
+    def post(self, request):
+        email = request.data.get('email', None)
+        password = request.data.get('password', None)
+
+        user = authenticate(username=email, password=password)
+
+        if user:
+            serializer = self.serializer_class(user)
 
