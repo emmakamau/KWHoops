f268c15f1e37fd23a2782f6fde5d4c3e6a602a9b
rossyvee <roseakinyi001@gmail.com>
2022-07-05 08:46:22 +0300

    updated the urls pattern to include auth

---
+++ wkhoops/authentication/urls.py
@@ -0,0 +1,7 @@
+from atexit import register
+from authentication import views
+from django.urls import path
+
+urlpatterns=[
+  path('register/',views.RegisterAPIView.as_view(), name="register")  
+]
