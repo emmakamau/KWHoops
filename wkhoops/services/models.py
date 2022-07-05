7b5fcba127f4da4317f6f9d2166196f458aa92fc
Fridah-kalee <fridahkalee@gmail.com>
2022-07-05 09:58:15 +0300

    create category class

--- wkhoops/services/models.py
+++ wkhoops/services/models.py
@@ -2,18 +2,27 @@ from django.db import models
 
 # Create your models here.
 class Services(models.Model):
-    CATEGORY=()
+    CATEGORY=(
+        ('Trainings'),
+        ('Yoga'),
+        ('Workouts'),
+        ('Diet'),
+        ('Bootcamp'),
+        ('Tournament'),
+    )
+
+    category = models.ForeignKey('Category',on_delete=models.CASCADE)
     description = models.TextField(max_length=200,null=True,default='')
     entry_date = models.DateField(auto_now_add=True,null=False)
     image = models.FileField(null=True)
     video = models.FileField(null=True)
-    category = models.ForeignKey('Category',on_delete=models.CASCADE)
+    
 
     def save_services(self):
         self.save()
 
     def __str__(self):
-        return self.description
+        return self.category
 
 class Category(models.Model):
     category_name = models.CharField(max_length=50,null=False)
