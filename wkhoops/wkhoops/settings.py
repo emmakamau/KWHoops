3a1132fccfb680a30ca2f8e7a1cfec2ed2a88a14
Fridah-kalee <fridahkalee@gmail.com>
2022-07-05 08:57:27 +0300

    creating models

--- wkhoops/wkhoops/settings.py
+++ wkhoops/wkhoops/settings.py
@@ -79,8 +79,10 @@ WSGI_APPLICATION = 'wkhoops.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'wkhoops',
+        'USER': 'fridah',
+        'PASSWORD':'kalee',
     }
 }
 
@@ -122,3 +124,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
