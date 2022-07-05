99f21a78a4a4805d279c8fb1d56425a298ed213e
faith62 <phifeichemu@gmail.com>
2022-07-05 11:47:54 +0300

    add subscription plan views

--- wkhoops/subscriptions/serializers.py
+++ wkhoops/subscriptions/serializers.py
@@ -2,7 +2,7 @@ from dataclasses import field
 from rest_framework import serializers
 from .models import Subscription_Plan
 
-class SnippetSerializer(serializers.ModelSerializer):
+class SubscriptionPlanSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Subscription_Plan
--- wkhoops/subscriptions/views.py
+++ wkhoops/subscriptions/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from rest_framework import generics
+from .models import Subscription_Plan
+from .serializers import SubscriptionPlanSerializer
 # Create your views here.
+
+class SubscriptionPlanList(generics.ListCreateAPIView):
+    queryset = Subscription_Plan.objects.all()
+    serializer_class=SubscriptionPlanSerializer
+class SubscriptionPlanDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Subscription_Plan.objects.all()
+    serializer_class=SubscriptionPlanSerializer    
