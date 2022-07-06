9b1ba07d472b19fb683bb5f2c7b403af6ff6e514
EmmaK <emmaculatewkamau@gmail.com>
2022-07-06 07:04:11 +0300

    Add connection between api and client side

--- frontend/src/app/user-authentication/user-authentication.component.ts
+++ frontend/src/app/user-authentication/user-authentication.component.ts
@@ -1,25 +1,53 @@
 import { Component, OnInit } from '@angular/core';
 import { UserAuthenticationService } from 'src/app/user-authentication.service';
-import { FormGroup, FormBuilder } from '@angular/forms';
+import { FormGroup, FormControl } from '@angular/forms';
+import { first } from 'rxjs/operators';
 
 declare var loadExtJS: any;
 
 @Component({
   selector: 'app-user-authentication',
   templateUrl: './user-authentication.component.html',
-  styleUrls: ['./user-authentication.component.css']
+  styleUrls: ['./user-authentication.component.css'],
+  providers: [UserAuthenticationService]
 })
 export class UserAuthenticationComponent implements OnInit {
 
-  public userSignInForm: FormGroup
+  // public userSignInForm: FormGroup
 
-  constructor(
-    private service:UserAuthenticationService) { }
+  constructor(private service:UserAuthenticationService) { }
 
+  register; 
+  login;
 
   ngOnInit(): void {
     new loadExtJS();
 
-    
+      this.register = {
+        username:'',
+        email:'',
+        password:''
+      };
+
+      this.login = {
+        email:'',
+        password:''
+      }
+  }
+
+  registerUser(){
+    this.service.registerUser(this.register).subscribe(response => {
+      alert('User'+this.register.username+'has been created')
+    },
+    error => console.log('error', error)
+    );
+  }
+
+  loginUser(){
+    this.service.loginUser(this.login).subscribe(response => {
+      alert('User'+this.login.username+'has been signed in')
+    },
+    error => console.log('error', error)
+    );
   }
 }
